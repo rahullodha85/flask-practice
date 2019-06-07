60b93eab469bf4a900a0a2e2415bfdce79824498
Rahul Lodha <rahul@152.sub-97-40-145.myvzw.com>
2019-06-07 15:42:39 -0400

    get and create user routes

--- app/user/routes.py
+++ app/user/routes.py
@@ -1,8 +1,52 @@
-from flask import Blueprint
+import json
+import uuid
 
-user = Blueprint('user', __name__)
+from flask import Blueprint, request, jsonify
+from werkzeug.security import generate_password_hash
+
+from app import db
+from app.user.models import User
+
+user_controller = Blueprint('user', __name__)
 
 
-@user.route('/')
-def index():
-    return 'Hello Blueprint!'
+@user_controller.route('', methods=['GET'])
+def get_all_users():
+    users = User.query.all()
+    output = []
+    for user in users:
+        output.append(user.serialize)
+    return jsonify({'users': output})
+
+
+@user_controller.route('/<public_id>', methods=['GET'])
+def get_one_user(public_id):
+    user = User.query.filter_by(public_id=public_id).first()
+
+    if not user:
+        return jsonify({'message': 'No user found!'}), 400
+
+    return jsonify({'user': user.serialize})
+
+
+@user_controller.route('', methods=['POST'])
+def create_user():
+    data = request.json
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+    test = User(**json.loads(json.dumps(request.json)))
+    test.public_id = str(uuid.uuid4())
+    test.password = hashed_password
+    test.admin = False
+    db.session.add(test)
+    db.session.commit()
+    return ''
+
+
+@user_controller.route('/<public_id>', methods=['PUT'])
+def update_user():
+    return ''
+
+
+@user_controller.route('/<public_id>', methods=['DELETE'])
+def delete_user():
+    return ''
--- run.py
+++ run.py
@@ -4,13 +4,18 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 from app import  app
-from app.user.routes import user
+from app.user.routes import user_controller
 
 
 # db = SQLAlchemy(app)
 # migrate = Migrate(app, db)
 
-app.register_blueprint(user)
+app.register_blueprint(user_controller, url_prefix ='/user')
+
+
+@app.route('/')
+def index():
+    return 'Home Page!'
 
 if __name__ == '__main__':
     app.run()
